7cad4b754a25b7fbd77130624dcdd8165fcbdb12
Zabaglione <3518103+zabaglione@users.noreply.github.com>
2026-09-11 22:17:38 +0900

    Distinguish closed SWITCH MAZE doors with inverse A and B labels

--- games/switch-maze/generate.py
+++ games/switch-maze/generate.py
@@ -16,8 +16,8 @@ for n in range(12):
     elif n in (2,3):b.rect(1,0,4,4);b.line(4,3,4,7);b.line(4,6,6,6)
     elif n in (4,5):b.text('A' if n==4 else 'B',1,0);b.dot(0,7);b.dot(7,7)
     elif n in (6,7):
-        b.line(0,0,0,7);b.line(7,0,7,7);b.line(0,7,7,7)
-        b.text('A' if n==6 else 'B',1,0)
+        b.rect(0,0,8,8,1,True)
+        b.text('A' if n==6 else 'B',1,0,c=0)
     elif n==8:b.rect(1,0,6,8);b.text('E',2,0)
     elif n in (9,10):
         b.line(0,0,7,0);b.line(0,7,7,7);b.dot(0,3);b.dot(7,4)
